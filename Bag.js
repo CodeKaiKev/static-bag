73003908ce8b87e35b2973c783a6bb272aefc73e
CodeKaiKev <CodeKaiKev@gmail.com>
2023-02-13 22:36:42 +0000

    KevinisAwesome

--- Bag.js
+++ Bag.js
@@ -1,8 +1,12 @@
 class Bag {
+    #maxWeight = 23;
     constructor(weight) {
         if(!weight){
             throw new Error('bag needs weight')
         } 
+        if(weight > this.#maxWeight) {
+            throw new Error('Bag needs to be less than 23kg');
+        }
         this.weight = weight
     }
 }
--- Bag.test.js
+++ Bag.test.js
@@ -3,11 +3,15 @@ const Bag = require('./Bag')
 describe('Bag class', () => {
     //bag has weight
     test('bag has weight', () => {
-        const bag = new Bag(25)
-        expect(bag.weight).toBe(25)
+        const bag = new Bag(19)
+        expect(bag.weight).toBe(19)
     })
     //if bag has no weight, return error
     test('bag has no weight to return an error', () => {
         expect(() => new Bag()).toThrowError('bag needs weight')
+    })
+    //if bag has no weight, return error
+    test('bag more than max weight', () => {
+        expect(() => new Bag(24)).toThrowError('Bag needs to be less than 23kg')
     })
 })
